1fd3f2afa7b96fb4cfb8cd46eaf40ef9e908a066
cv-bit <ververcameron29@gmail.com>
2021-07-27 19:33:58 -0500

    update sign up page

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -2,5 +2,20 @@ import React from 'react';
 import '../../App.css';
 
 export default function SignUp() {
-    return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+    return (
+        <>
+            <form action="action_page.php" className='sign-up'>
+                <div className="container">
+                    <h1>SIGN UP</h1>
+                    <p>to hear about our latest packages</p>
+                    
+                    <label for="email"><b>Email: </b></label>
+                    <input type="text" placeholder="Enter Email" name="email" required></input>
+                    <div class="clearfix">
+                        <button type="submit" class="signupbtn">SIGN UP</button>
+                    </div>
+                </div>
+            </form>
+        </>
+    );
 }
